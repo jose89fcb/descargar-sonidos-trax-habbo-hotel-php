57cb0fea479af826937bda4a57dcecc85ec4686e
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-11-11 19:49:17 +0100

    Add files via upload

---
+++ sonidos trax habbohotel.php
@@ -0,0 +1,6 @@
+<?php
+  for($a = 1; $a <= 736; $a++){
+      $url = 'http://images.habbo.com/dcr/hof_furni//mp3/sound_machine_sample_'.$a.'.mp3';
+      echo '<a href="'.$url.'">'.$url.'</a><br />';
+}
+?>
